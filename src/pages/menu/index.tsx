0401af1112a7ee3b098d17d3cb0e49917cd3aa7d
Hendri Alqori <teamhendri18@gmail.com>
2024-12-17 21:25:09 +0700

    fix: remove image client

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom"
 import Layout from "../../components/layout";
 import { useGetImages } from "../../services/menu-service";
-import { IMAGES } from "../../constant";
+// import { IMAGES } from "../../constant";
 
 export default function Menu() {
     const { menu, submenu, type } = useParams()
 
-    const imagesClient = IMAGES[submenu as keyof typeof IMAGES][type as "arabic" | "english"]
+    // const imagesClient = IMAGES[submenu as keyof typeof IMAGES][type as "arabic" | "english"]
     const imagesServer = useGetImages({ menu: String(menu), submenu: String(submenu), type: String(type) })
     
     return (
@@ -18,9 +18,9 @@ export default function Menu() {
                     <div className="relative z-1 w-[calc(100%_-_50px)] max-w-7xl mx-auto space-y-5 md:space-y-10 pt-24 md:pt-40 pb-24">
                         <h1 className="text-white font-bold text-lg md:text-2xl text-center">{menu?.toLocaleUpperCase()} - {submenu?.toLocaleUpperCase()} - {type?.toLocaleUpperCase()}</h1>
                         <div className="flex flex-wrap justify-center items-start gap-2">
-                            {imagesClient?.map((image, i) => (
+                            {/* {imagesClient?.map((image, i) => (
                                 <img key={i} src={image} alt={`${submenu}-image`} className="w-full md:w-1/3 rounded-2xl" loading="lazy" />
-                            ))}
+                            ))} */}
                             {imagesServer.data?.data.map((image, i) => (
                                 <img key={i} src={image.cloudUrl} alt={`${submenu}-image`} className="w-full md:w-1/3 rounded-2xl" loading="lazy" />
                             ))}
